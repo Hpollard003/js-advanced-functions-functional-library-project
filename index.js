d99c7598db995d73cd3843b966d9cd668737a63a
Hpollard003 <hpollard003@gmail.com>
2021-04-26 13:04:09 -0400

    Done.

--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const fi = (function () {
     libraryMethod: function () {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
-
     each: function (collection, callback) {
       const collector = collection instanceof Array ? collection.slice() : Object.values(collection)
       for (let i = 0; i < collector.length; i++)
         callback(collector[i])
       return collection
     },
-
     map: function (collection, callback) {
       const collector = collection instanceof Array ? collection.slice() : Object.values(collection)
       const newArr = []
@@ -19,28 +17,33 @@ const fi = (function () {
       };
       return newArr
     },
-
-    reduce: function (collection, callback, acc) {
-      const values = collection.slice(0)
-      for (let i = 0; i < values.length; i++) {
-        return acc = callback(acc, values[i], collection)
+    reduce: function (array = [], callback = () => {}, acc) {
+      let collection = array.slice(0)
+      if (!acc) {
+        acc = collection[0]
+        collection = collection.slice(1)
+      }
+      for(let i = 0; i<collection.length; i++) {
+      acc = callback(acc, collection[i], collection)
       }
       return acc
     },
+    find: function (collection, predicate) {
+      console.log('collection =',collection, '    ', 'predicate =', predicate)
+    },
 
-    find: function () { },
-    filter: function () { },
-    size: function () { },
-    first: function () { },
-    last: function () { },
-    compact: function () { },
-    sortBy: function () { },
-    flatten: function () { },
-    uniq: function () { },
-    keys: function () { },
-    values: function () { },
-    functions: function () { },
+  filter: function () { },
+  // size: function () { },
+  // first: function () { },
+  // last: function () { },
+  // compact: function () { },
+  // sortBy: function () { },
+  // flatten: function () { },
+  // uniq: function () { },
+  // keys: function () { },
+  // values: function () { },
+  // functions: function () { },
   }
-})()
+}) ()
 
 fi.libraryMethod()
