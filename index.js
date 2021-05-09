d3d45a199c7f6f3038a7faca7d38e96a5eed0cd2
Hpollard003 <hpollard003@gmail.com>
2021-05-09 15:38:09 -0400

    commit tree 12a17d369769755aaaa3a80b88be891f242d9686

--- index.js
+++ index.js
@@ -17,33 +17,68 @@ const fi = (function () {
       };
       return newArr
     },
-    reduce: function (array = [], callback = () => {}, acc) {
+    reduce: function (array = [], callback = () => { }, acc) {
       let collection = array.slice(0)
       if (!acc) {
         acc = collection[0]
         collection = collection.slice(1)
       }
-      for(let i = 0; i<collection.length; i++) {
-      acc = callback(acc, collection[i], collection)
+      for (let i = 0; i < collection.length; i++) {
+        acc = callback(acc, collection[i], collection)
       }
       return acc
     },
     find: function (collection, predicate) {
-      console.log('collection =',collection, '    ', 'predicate =', predicate)
+      // console.log(collection)
+      const collector = collection instanceof Array ? collection : Object.values(collection)
+      // console.log(collector)
+      for (let i = 0; i < collector.length; i++) {
+        const finder = predicate(collector[i])
+        if (finder) {
+          return (collector[i])
+        }
+      }
+      // return(find)
     },
+    filter: function (collection, predicate) {
+      const array = []
+      const collector = collection instanceof Array ? collection : Object.values(collection)
+      collector.forEach(x => {
+        const finder = predicate(x)
+        if (finder) {
+          array.push(x)
+        }
+      })
 
-  filter: function () { },
-  // size: function () { },
-  // first: function () { },
-  // last: function () { },
-  // compact: function () { },
-  // sortBy: function () { },
-  // flatten: function () { },
-  // uniq: function () { },
-  // keys: function () { },
-  // values: function () { },
-  // functions: function () { },
+      // array.push(thing)
+      return (array)
+    },
+    size: function (collection) {
+      const collector = collection instanceof Array ? collection : Object.values(collection)
+      return collector.length
+    },
+    first: function (collection, n) {
+      let pos = 0
+      const collector = collection instanceof Array ? collection : Object.values(collection)
+      if (n > 0) {
+        return collector.splice(pos, n)
+      } else {
+        return collector[0]
+      }
+    },
+    last: function (array, n) {
+      return array[n]
+
+
+     },
+    // compact: function () { },
+    // sortBy: function () { },
+    // flatten: function () { },
+    // uniq: function () { },
+    // keys: function () { },
+    // values: function () { },
+    // functions: function () { },
   }
-}) ()
+})()
 
 fi.libraryMethod()
